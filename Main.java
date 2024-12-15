2d513a6fa100db5648fd12854be83438a0c11985
L0G4N2 <136356225+L0G4N2@users.noreply.github.com>
2024-12-14 18:14:12 -0600

    Added a Scrap File

--- Main.java
+++ Main.java
@@ -1,66 +1,19 @@
 import java.util.Scanner;
 public class Main {
-
-    public static double AverageStaticFriction(int numOfTrials, boolean needsConversion) {
-        Scanner scan = new Scanner(System.in);
-        double staticFriction = 0;
-        if (needsConversion == false) {
-            for (int i = 1; i <= numOfTrials; i++) {
-                System.out.println("Enter the masses from Trial " + i + ".");
-                System.out.println("Enter the first mass: ");
-                double mass1 = scan.nextDouble();
-                System.out.println("Enter the second mass: ");
-                double mass2 = scan.nextDouble();
-                staticFriction += (mass2 / mass1);
-            }
-        }
-        else if (needsConversion == true) {
-            System.out.println("Which column needs to be converted? 1, 2, or Both(3)?");
-            String whichColumn = scan.nextLine();
-            if (whichColumn.equals("1")) {
-                for (int i = 1; i <= numOfTrials; i++) {
-                    System.out.println("Enter the masses from Trial " + i + ".");
-                    System.out.println("Enter the first mass: ");
-                    double mass1 = scan.nextDouble();
-                    System.out.println("Enter the second mass: ");
-                    double mass2 = scan.nextDouble();
-                    staticFriction += ((mass2) / (mass1 / 1000));
-                }
-            }
-            else if (whichColumn.equals("2")) {
-                for (int i = 1; i <= numOfTrials; i++) {
-                    System.out.println("Enter the masses from Trial " + i + ".");
-                    System.out.println("Enter the first mass: ");
-                    double mass1 = scan.nextDouble();
-                    System.out.println("Enter the second mass: ");
-                    double mass2 = scan.nextDouble();
-                    staticFriction += ((mass2 / 1000) / (mass1));
-                }
-            }
-            else if (whichColumn.toUpperCase().equals("BOTH") || whichColumn.equals("3")) {
-                for (int i = 1; i <= numOfTrials; i++) {
-                    System.out.println("Enter the masses from Trial " + i + ".");
-                    System.out.println("Enter the first mass: ");
-                    double mass1 = scan.nextDouble();
-                    System.out.println("Enter the second mass: ");
-                    double mass2 = scan.nextDouble();
-                    staticFriction += ((mass2 / 1000) / (mass1 / 1000));
-                }
-            }
-        }
-        return (staticFriction / numOfTrials);
-    }
     
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        Scrap scrap = new Scrap();
+        AverageStaticFriction avgStatFriction = new AverageStaticFriction();
+
         System.out.println("Do you need help calculating static friction? (y/n)");
         String needCalculator = scan.nextLine();
-        if (needCalculator.equals("y")) {
+        if (needCalculator.equals("y") || needCalculator.equals("Y")) {
             System.out.println("How many trials did you complete?");
             int numOfTrials = scan.nextInt();
             System.err.println("Does your data need conversion? (true/false)");
             boolean needsConversion = scan.nextBoolean();
-            System.out.println(AverageStaticFriction(numOfTrials, needsConversion));
+            System.out.println(avgStatFriction.calculate(numOfTrials, needsConversion));
         }
         System.out.println("Is there anything else you need? (y/n)");
         String needHelp = scan.nextLine();
@@ -74,9 +27,14 @@ public class Main {
                 break;
             }
             else {
-                System.out.println("I do not understand your response...");
+                System.out.println("Waiting on a valid response...");
                 needHelp = scan.nextLine();
             }
         }
+        System.out.println("If you would like to run anything else: Run \"scrap.trash.\"");
+        System.out.println("Freely edit \"Scrap.java\" before running it.");
+        System.out.println("Next, you\'d want to call the function from the \"Scrap\" class in the code.");
+        // Uncomment the next line either by deleting the "//" or by selecting the line and pressing "CTRL + /"
+        // scrap.trash();
     }
 }
---
+++ Scrap.java
@@ -0,0 +1,5 @@
+public class Scrap {
+    public void trash() {
+        /* This is where you would type your code */
+    }
+}
