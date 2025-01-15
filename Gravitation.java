33bf869c0233ce4d1122fbb4749e2475444d5a37
L0G4N2 <136356225+L0G4N2@users.noreply.github.com>
2025-01-14 19:02:41 -0600

    Added a Gravitation Comparer

---
+++ Gravitation.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Gravitation {
+    public double force(double m1, double m2, double r) {
+        double G = (6.674) * (10 ^ -11);
+        return(G * ((m1 * m2)/(r * r)));
+    }
+    public ArrayList<Double> calculate(int numOfMasses) {
+        Scanner scan = new Scanner(System.in);
+        ArrayList<Double> forces = new ArrayList<Double>();
+        for (int i = 1; i <= numOfMasses; i++) {
+            System.out.println("System " + i + ":");
+            System.out.print("Enter the first mass: ");
+            double m1 = scan.nextDouble();
+            System.out.println();
+            System.out.print("Enter the second mass: ");
+            double m2 = scan.nextDouble();
+            System.out.println();
+            System.out.print("Enter the distance between the masses: ");
+            double r = scan.nextDouble();
+            forces.add(force(m1, m2, r));
+        }
+        return(forces);
+    }
+}
--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         Scrap scrap = new Scrap();
+        Gravitation grav = new Gravitation();
         AverageStaticFriction avgStatFriction = new AverageStaticFriction();
 
         System.out.println("Do you need help calculating static friction? (y/n)");
@@ -11,10 +12,19 @@ public class Main {
         if (needCalculator.equals("y") || needCalculator.equals("Y")) {
             System.out.println("How many trials did you complete?");
             int numOfTrials = scan.nextInt();
-            System.err.println("Does your data need conversion? (true/false)");
+            System.out.println("Does your data need conversion? (true/false)");
             boolean needsConversion = scan.nextBoolean();
             System.out.println(avgStatFriction.calculate(numOfTrials, needsConversion));
         }
+
+        System.out.println("Do you need help comparing the gravitation between objects? (y/n)");
+        String needGravitation = scan.nextLine();
+        if (needGravitation.equals("y") || needGravitation.equals("Y")) {
+            System.out.println("How many systems are there?");
+            int numOfMasses = scan.nextInt();
+            System.out.println(grav.calculate(numOfMasses));
+        }
+
         System.out.println("Is there anything else you need? (y/n)");
         String needHelp = scan.nextLine();
         while (!(needHelp.equals("y") || needHelp.equals("n"))) {
@@ -31,6 +41,7 @@ public class Main {
                 needHelp = scan.nextLine();
             }
         }
+        
         System.out.println("If you would like to run anything else: Run \"scrap.trash.\"");
         System.out.println("Freely edit \"Scrap.java\" before running it.");
         System.out.println("Next, you\'d want to call the function from the \"Scrap\" class in the code.");
