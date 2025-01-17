bea54c8a1a7262a1461e01446f19e995b7ad44ba
L0G4N2 <136356225+L0G4N2@users.noreply.github.com>
2025-01-16 20:44:43 -0600

    Finalized the Gravitation Calculator

--- Gravitation.java
+++ Gravitation.java
@@ -3,24 +3,97 @@ import java.util.Scanner;
 
 public class Gravitation {
     public double force(double m1, double m2, double r) {
-        double G = (6.674) * (10 ^ -11);
+        double G = (6.674) * (Math.pow(10, -11));
         return(G * ((m1 * m2)/(r * r)));
     }
-    public ArrayList<Double> calculate(int numOfMasses) {
+    public ArrayList<Double> calculate(int numOfSystems, boolean isVariables, boolean sameMass) {
         Scanner scan = new Scanner(System.in);
         ArrayList<Double> forces = new ArrayList<>();
-        for (int i = 1; i <= numOfMasses; i++) {
-            System.out.println("System " + i + ":");
-            System.out.print("Enter the first mass: ");
+        if (isVariables == false && sameMass == false) {
+            for (int i = 1; i <= numOfSystems; i++) {
+                System.out.println("System " + i + ":");
+                System.out.print("Enter the first mass: ");
+                double m1 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the second mass: ");
+                double m2 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the distance between the masses: ");
+                double r = scan.nextDouble();
+                forces.add(force(m1, m2, r));
+            }
+            return(forces);
+        }
+        else if (isVariables == false && sameMass == true) {
+            System.out.print("Enter the mass that is the same throughout all the systems: ");
             double m1 = scan.nextDouble();
             System.out.println();
-            System.out.print("Enter the second mass: ");
-            double m2 = scan.nextDouble();
-            System.out.println();
-            System.out.print("Enter the distance between the masses: ");
-            double r = scan.nextDouble();
-            forces.add(force(m1, m2, r));
+            for (int i = 1; i <= numOfSystems; i++) {
+                System.out.println("System " + i + ":");
+                System.out.print("Enter the mass of the second object: ");
+                double m2 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the distance between the masses: ");
+                double r = scan.nextDouble();
+                forces.add(force(m1, m2, r));
+            }
+            return(forces);
         }
-        return(forces);
+        else if (isVariables == true && sameMass == true) {
+            System.out.print("Enter the factor of the mass that is the same throughout all the systems: ");
+            double f1 = scan.nextDouble();
+            System.out.println();
+            for (int i = 1; i <= numOfSystems; i++) {
+                System.out.println("System " + i + ":");
+                System.out.print("Enter the factor of the mass of the second object: ");
+                double f2 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the factor of the distance between the masses: ");
+                double fr = scan.nextDouble();
+                forces.add(force((3 * f1), (4 * f2), (5 * fr)));
+            }
+            return(forces);
+        }
+        else if (isVariables == true && sameMass == false) {
+            for (int i = 1; i <= numOfSystems; i++) {
+                System.out.println("System " + i + ":");
+                System.out.print("Enter the factor of the first mass: ");
+                double f1 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the factor of the second mass: ");
+                double f2 = scan.nextDouble();
+                System.out.println();
+                System.out.print("Enter the factor of the distance between the masses: ");
+                double fr = scan.nextDouble();
+                forces.add(force((3 * f1), (4 * f2), (5 * fr)));
+            }
+            return(forces);
+        }
+        else {
+            return(null);
+        }
+    }
+    public ArrayList<Double> sortNumList(ArrayList<Double> list) {
+        ArrayList<Double> temp = new ArrayList<>(list);
+        int size = temp.size();
+        ArrayList<Double> sortedList = new ArrayList<>();
+        while (sortedList.size() != size) {
+            double greatestNum = 0;
+            if (temp.size() == 1) {
+                greatestNum = temp.get(0);
+                sortedList.add(greatestNum);
+                temp.remove(greatestNum);
+            }
+            else {
+                for (int i = 0; i < temp.size(); i++) {
+                    if (temp.get(i) > greatestNum) {
+                        greatestNum = temp.get(i);
+                    }
+                }
+                sortedList.add(greatestNum);
+                temp.remove(greatestNum);
+            }
+        }
+        return(sortedList);
     }
 }
--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     
@@ -7,9 +8,9 @@ public class Main {
         Gravitation grav = new Gravitation();
         AverageStaticFriction avgStatFriction = new AverageStaticFriction();
 
-        System.out.println("Do you need help calculating static friction? (y/n)");
-        String needCalculator = scan.nextLine();
-        if (needCalculator.equals("y") || needCalculator.equals("Y")) {
+        System.out.println("Do you need help calculating static friction? (Yes/No)");
+        String needCalculator = scan.nextLine().toUpperCase();
+        if (needCalculator.equals("Y") || needCalculator.equals("YES")) {
             System.out.println("How many trials did you complete?");
             int numOfTrials = scan.nextInt();
             System.out.println("Does your data need conversion? (true/false)");
@@ -17,22 +18,49 @@ public class Main {
             System.out.println(avgStatFriction.calculate(numOfTrials, needsConversion));
         }
 
-        System.out.println("Do you need help comparing the gravitation between objects? (y/n)");
-        String needGravitation = scan.nextLine();
-        if (needGravitation.equals("y") || needGravitation.equals("Y")) {
+        System.out.println("Do you need help comparing the gravitation between objects? (Yes/No)");
+        String needGravitation = scan.nextLine().toUpperCase();
+        if (needGravitation.equals("Y") || needGravitation.equals("YES")) {
             System.out.println("How many systems are there?");
-            int numOfMasses = scan.nextInt();
-            System.out.println(grav.calculate(numOfMasses));
+            int numOfSystems = scan.nextInt();
+            System.out.println("Are the masses numerical or variable? (1 or 2)");
+            boolean isVariables = true;
+            int temp = scan.nextInt();
+            if (temp == 1) {
+                isVariables = false;
+            }
+            else if (temp == 2) {
+                isVariables = true;
+            }
+            System.out.println("Is there one mass that is the same throughout all the systems? (Yes/No)");
+            String sameMassAnswer = scan.nextLine().toUpperCase();
+            boolean sameMass = false;
+            while (!(sameMassAnswer.equals("Y") || sameMassAnswer.equals("YES"))) {
+                sameMassAnswer = scan.nextLine().toUpperCase();
+            }
+            if (sameMassAnswer.equals("Y") || sameMassAnswer.equals("YES")) {
+                sameMass = true;
+            }
+            ArrayList <Double> list = new ArrayList<>(grav.calculate(numOfSystems, isVariables, sameMass));
+            System.out.println(list);
+            System.out.println("Sort this data? (Yes/No)");
+            String sort = scan.nextLine().toUpperCase();
+            while (!(sort.equals("Y") || sort.equals("YES"))) {
+                sort = scan.nextLine().toUpperCase();
+            }
+            if (sort.equals("Y")) {
+                System.out.println(grav.sortNumList(list));
+            }
         }
 
         System.out.println("Is there anything else you need? (y/n)");
-        String needHelp = scan.nextLine();
-        while (!(needHelp.equals("y") || needHelp.equals("n"))) {
-            if (needHelp.equals("y")) {
+        String needHelp = scan.nextLine().toUpperCase();
+        while (!(needHelp.equals("Y") || needHelp.equals("N"))) {
+            if (needHelp.equals("Y")) {
                 System.out.println("What can I help with?");
                 break;
             }
-            else if (needHelp.equals("n")) {
+            else if (needHelp.equals("N")) {
                 System.out.println("Well, bye bye then!");
                 break;
             }
@@ -41,7 +69,7 @@ public class Main {
                 needHelp = scan.nextLine();
             }
         }
-        
+
         System.out.println("If you would like to run anything else: Run \"scrap.trash.\"");
         System.out.println("Freely edit \"Scrap.java\" before running it.");
         System.out.println("Next, you\'d want to call the function from the \"Scrap\" class in the code.");
