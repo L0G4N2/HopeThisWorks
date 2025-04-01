3937780ac74b4b6765ee66cbd3b3e9db5df47d01
L0G4N2 <136356225+L0G4N2@users.noreply.github.com>
2025-03-31 20:50:27 -0500

    Some Autonomous

--- VEX_SillyStuff/RedLightGreenLight-2025-03-26T01-03-28/src/main.cpp
+++ VEX_SillyStuff/RedLightGreenLight-2025-03-26T01-03-28/src/main.cpp
@@ -21,12 +21,48 @@ using namespace vex;
 
 int turnPosition = 0;
 
+void greenLight() {
+  Motor11.spin(forward);
+  wait(380, msec);
+  Motor11.stop(hold);
+}
+
+void redLight() {
+  Motor11.spin(reverse);
+  wait(380, msec);
+  Motor11.stop(hold);
+}
+
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
+  // srand(Brain.Timer.systemHighResolution());
+  // int randomNumber = rand() % 5 + 1;
   Motor11.setVelocity(50, percent);
   Rotation12.setPosition(0, degrees);
   Rotation12.resetPosition();
+  // switch (randomNumber) {
+  //   case 1:
+  //     greenLight();
+  //     wait(750, msec);
+  //     redLight();
+  //   case 2:
+  //     greenLight();
+  //     wait(1000, msec);
+  //     redLight();
+  //   case 3:
+  //     greenLight();
+  //     wait(1250, msec);
+  //     redLight();
+  //   case 4:
+  //     greenLight();
+  //     wait(1900, msec);
+  //     redLight();
+  //   case 5:
+  //     greenLight();
+  //     wait(2000, msec);
+  //     redLight();
+  // }
   while (true) {
     wait(250, msec);
     if (Controller1.ButtonA.pressing() && (Rotation12.angle(degrees) >= 335 || Rotation12.angle(degrees) < 25)) {
