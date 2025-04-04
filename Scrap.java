5bcac177b487b1c8e7cb7fd116ca7ac1768e15e0
L0G4N2 <136356225+L0G4N2@users.noreply.github.com>
2025-04-04 11:07:33 -0500

    Added a Main Function to Scrap

--- Scrap.java
+++ Scrap.java
@@ -1,5 +1,10 @@
 public class Scrap {
+
     public void trash() {
         /* This is where you would type your code */
+    }
+
+    public static void main(String[] args) {
+        /* Code to Run WITHOUT Running in Main */
     }
 }
