02c660f23fa49584fa5e215eb44f425d504c21a1
L0G4N2 <loganjgutierrez@gmail.com>
2026-08-20 21:32:46 -0500

    Updates

--- Java_SillyStuff/src/Scrap.java
+++ Java_SillyStuff/src/Scrap.java
@@ -8,3 +8,14 @@ public class Scrap {
         /* Code to Run WITHOUT Running in Main */
     }
 }
+
+public class dumpster extends Scrap {
+
+    public void trash() {
+        /* This is where you would type your code */
+    }
+
+    public static void main(String[] args) {
+        /* Code to Run WITHOUT Running in Main */
+    }
+}
